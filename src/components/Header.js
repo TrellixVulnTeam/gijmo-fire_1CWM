eb287f008b26679071a066aae6abad53f8165c6b
EndritKane <tpdov.kpr@gmail.com>
2018-02-26 16:41:25 -0500

    Updated Header to Include a Gigs Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
   return (
     <header className="top">
       <h3 className="tagline">Website: {props.websiteId}</h3>
-      <h3>Collections: <a href={'/website/' + props.websiteId + '/artists'}>Artists</a>  <a href={'/website/' + props.websiteId + '/artists'}>Events</a></h3>
+      <h3>Collections: <a href={'/website/' + props.websiteId + '/artists'}>Artists</a>  <a href={'/website/' + props.websiteId + '/Gigs'}>Gigs</a></h3>
     </header>
   )
 }
